c2f7dac47437eee7c1e8cb778a79707bc5f6d42b
anand <anandmohan.amp@gmail.com>
2025-10-31 23:15:32 +0530

    rm530_5g_integration/__init__.py : Update version to 3.0.1 and export health monitoring

--- rm530_5g_integration/__init__.py
+++ rm530_5g_integration/__init__.py
@@ -4,7 +4,7 @@ RM530 5G Integration Package
 Integration tools for Waveshare RM530 5G modem with Raspberry Pi using ECM mode.
 """
 
-__version__ = "2.0.0"
+__version__ = "3.0.1"
 __author__ = "Anand"
 __email__ = "anand@example.com"
 __description__ = "Integration tools and scripts for Waveshare RM530 5G modem with Raspberry Pi"
@@ -13,6 +13,7 @@ __description__ = "Integration tools and scripts for Waveshare RM530 5G modem wi
 from rm530_5g_integration.core.manager import RM530Manager
 from rm530_5g_integration.core.modem import Modem
 from rm530_5g_integration.core.network import NetworkManager
+from rm530_5g_integration.core.health import HealthMonitor, HealthStatus
 from rm530_5g_integration.monitoring import SignalQuality, ConnectionStats
 
 __all__ = [
@@ -23,6 +24,8 @@ __all__ = [
     "RM530Manager",
     "Modem",
     "NetworkManager",
+    "HealthMonitor",
+    "HealthStatus",
     "SignalQuality",
     "ConnectionStats",
 ]
